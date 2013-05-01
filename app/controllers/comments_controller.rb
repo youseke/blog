17832fa723ca3a666eb3288d9ef6ec06476b20a7
youseke <tohtetsu.choh@nts.ricoh.co.jp>
2013-05-01 16:07:28 +0900

    test rebase

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,13 +6,12 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
   end
 
-   def destroy
+  def destroy
+    mail = UserMailer.send_mail
+    mail.deliver
     @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
     @comment.destroy
-    mail = UserMailer.send_mail
-    mail.deliver
     redirect_to post_path(@post)
   end
-
 end
